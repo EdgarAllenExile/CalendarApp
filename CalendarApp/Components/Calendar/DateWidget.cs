96e0edd392aa40090eb61082a07d448bae91daea
Riley Mercer <riley.mercer4@gmail.com>
2024-05-10 23:19:30 +0800

    Vaugely functional dating system

---
+++ CalendarApp/Components/Calendar/DateWidget.cs
@@ -0,0 +1,12 @@
+﻿namespace CalendarApp.Components.Calendar
+{
+    public class DateWidget
+    {
+        public DateTime SelectedDate{ get; set; } = DateTime.Now;
+    }
+    public class Days
+    {
+        public DateOnly DateOfMonth { get; set; }    
+
+    }
+}
